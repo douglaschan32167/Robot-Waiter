04dd7744277665776e75f68128207420d349f2c9
Douglas Chan <douglaschan32167@berkeley.edu>
2013-11-20 22:43:42 -0800

    Can now manually enter target IP address.

--- voiceRecognition/OrderTaker/gen/com/example/ordertaker/R.java
+++ voiceRecognition/OrderTaker/gen/com/example/ordertaker/R.java
@@ -24,10 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int btnSpeak=0x7f080001;
-        public static final int killButton=0x7f080004;
-        public static final int sendOrderButton=0x7f080003;
+        public static final int ipText=0x7f080003;
+        public static final int killButton=0x7f080005;
+        public static final int sendOrderButton=0x7f080004;
         public static final int textView1=0x7f080000;
         public static final int txtText=0x7f080002;
     }
@@ -42,6 +43,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
         public static final int killButton=0x7f050004;
+        public static final int putIpHere=0x7f050006;
         public static final int sendOrder=0x7f050005;
         public static final int speaktext=0x7f050003;
     }
